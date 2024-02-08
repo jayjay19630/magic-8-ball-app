625d28626a0ffece81d480c87c35922e400e6267
positivelyjon <positivelyjon@gmail.com>
2024-02-09 01:05:21 +0800

    Initialise MUI Icon and React Hook Form Libraries to make query form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import './App.css'
 import EightBallSVG from './components/EightBall/EightBallSVG';
 import { Heading } from './components/Heading/Heading';
 import { SpeechBubble } from './components/SpeechBubble/SpeechBubble';
+import { UserInput } from './components/UserInput/UserInput';
 
 function App() {
   
+  const [response, setResponse] = useState('Ask me a yes or no question!');
+
+
   return (
     <div className='home'>
       <Heading/>
       <div className='ball-container'>
-        <SpeechBubble response={''}/>
+        <SpeechBubble response={response}/>
         <EightBallSVG />
+        <UserInput setResponse={setResponse}/>
       </div>
     </div>
   )
---
+++ src/components/UserInput/UserInput.tsx
@@ -0,0 +1,36 @@
+import { Button, Icon, Stack, TextField } from '@mui/material';
+import './UserInput.css'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { QuestionAnswer } from '@mui/icons-material';
+
+interface IUserInput {
+    query: string
+  }
+
+export const UserInput = (props: { setResponse: Function }) => {
+
+    const { register, handleSubmit, formState: { errors }} = useForm<IUserInput>();
+
+    const handleQuerySubmit: SubmitHandler<IUserInput> = (data: {query: string}) => {
+        props.setResponse(data.query);
+    }
+
+    return (
+        <form onSubmit={handleSubmit(handleQuerySubmit)}>
+            <Stack spacing={2} width={700} direction={'row'}>
+                <TextField 
+                    type="query" 
+                    {...register("query", {required: 'Query is required'})}
+                    placeholder='Query the ball...'
+                    error={!!errors.query}
+                    helperText={errors.query?.message}
+                    style={{ width: 3000 }}
+                />
+                <Button sx={{width:100, height: 50, backgroundColor: '#2c67f2'}} type='submit' variant='contained' disableTouchRipple>
+                    <QuestionAnswer/>
+                </Button>
+            </Stack>
+        </form>
+        
+    )
+}

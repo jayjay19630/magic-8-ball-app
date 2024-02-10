b3a7434d891c3d31c17a9caa2ca9a08af66f4495
positivelyjon <positivelyjon@gmail.com>
2024-02-10 13:55:00 +0800

    Create new component for home page and add routes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     define: {
-      'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY)
+      'process.env.OPENAI_API_KEY': JSON.stringify(env.OPENAI_API_KEY),
+      'process.env.USING_BACKEND': JSON.stringify(env.BACKEND)
     },
     plugins: [react()],
   }
